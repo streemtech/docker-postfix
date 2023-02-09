3eac3f42ae1e9e8df09e5c7c9b79935973c84b47
Bojan Čekrlić <bojan@cekrlic.com>
2023-02-09 09:02:28 +0100

    Fix for #118: Properly indent securityContext

--- helm/tests.sh
+++ helm/tests.sh
@@ -2,7 +2,13 @@
 set -e
 mkdir -p fixtures
 
-for i in `find -maxdepth 1 -type f -name test\*yml | sort`; do
+FIND="find"
+# Brew installs GNU find as "gfind" by default
+if command -v gfind >/dev/null 2>&2; then
+    FIND="$(which gfind)"
+fi
+
+for i in `${FIND} -maxdepth 1 -type f -name test\*yml | sort`; do
     echo "☆☆☆☆☆☆☆☆☆☆ $i ☆☆☆☆☆☆☆☆☆☆"
     helm template -f $i --dry-run mail > fixtures/demo.yaml
     docker run -it -v `pwd`/fixtures:/fixtures garethr/kubeval fixtures/demo.yaml
