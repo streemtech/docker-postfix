085746ea4dd687b818dac0418d1d89d33648c5fa
Bojan Čekrlić <bojan@cekrlic.com>
2025-01-30 20:18:42 +0100

    Fix: Try to fix building of postfix-exporter

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -5,7 +5,20 @@ if [ -f /tmp/container_is_terminating ]; then
 fi
 
 check_postfix() {
-    printf "EHLO healthcheck\nquit\n" | \
+    local proxy_protocol="$(postconf postscreen_upstream_proxy_protocol | cut -f2- -d= | tr -d '[:blank:]')"
+
+    check_string="EHLO healthcheck\nquit\n"
+
+    if [ -n "$proxy_protocol" ]; then
+        check_string="PROXY TCP4 127.0.0.1 127.0.0.1 587 587\n${check_string}"
+        #                   ^--- proxied internet protocol and family
+        #                        ^--- source address
+        #                                  ^--- destination address
+        #                                            ^--- source port
+        #                                                ^--- destination port
+    fi
+
+    printf "${check_string}" | \
     { while read l ; do sleep 1; echo $l; done } | \
     nc -w 2 127.0.0.1 587 | \
     grep -qE "^220.*ESMTP Postfix"
